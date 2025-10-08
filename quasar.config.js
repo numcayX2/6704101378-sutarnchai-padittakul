36e7b675b20709673ba29aa64a75ea601c786d03
numcayX2 <sokulshop5@gmail.com>
2025-10-08 11:54:46 +0700

    CI/CD

--- quasar.config.js
+++ quasar.config.js
@@ -36,6 +36,7 @@ export default defineConfig((/* ctx */) => {
         browser: ['es2022', 'firefox115', 'chrome115', 'safari14'],
         node: 'node20',
       },
+      publicPath: '/6704101378-sutarnchai-padittakul/',
 
       vueRouterMode: 'hash', // available values: 'hash', 'history'
       // vueRouterBase,
